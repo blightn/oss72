943612c0c070fb8b5623e054527b8def2cb7258a
blightn <blightan@gmail.com>
2021-03-27 20:10:28 +0300

    fix

--- oss72/settings.py
+++ oss72/settings.py
@@ -72,7 +72,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'Europe/Moscow'
 
